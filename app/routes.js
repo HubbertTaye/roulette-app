6348570cc7cd10e637f0f83855963d6d41538ff8
Tehya Hubbert <taye@Tehyas-MacBook-Pro.local>
2019-04-10 18:29:08 -0400

    server runs and routes.js set up

---
+++ app/routes.js
@@ -0,0 +1,77 @@
+module.exports = function(app, passport) { //add db when database.js is completed
+
+// render initial page ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); //add database render if needed be
+    });
+
+    // =============================================================================
+// GAME API SET UP =============================================================
+// =============================================================================
+
+//make note of what each route is doing
+
+    // =============================================================================
+// OWNER API SET UP ============================================================
+// =============================================================================
+
+//make note of what each route is doing
+
+    // =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+  // locally --------------------------------
+      // LOGIN ===============================
+      // show the login form
+      app.get('/login', function(req, res) {
+          res.render('login.ejs', { message: req.flash('loginMessage') });
+      });
+
+      // process the login form
+      app.post('/login', passport.authenticate('local-login', {
+          successRedirect : '/colorlog', // redirect to main page
+          failureRedirect : '/login', // redirect back to the signup page if there is an error
+          failureFlash : true // allow flash messages
+      }));
+
+      // SIGNUP =================================
+      // show the signup form
+      app.get('/signup', function(req, res) {
+          res.render('signup.ejs', { message: req.flash('signupMessage') });
+      });
+
+      // process the signup form
+      app.post('/signup', passport.authenticate('local-signup', {
+          successRedirect : '/colorlog', // redirect to main page
+          failureRedirect : '/signup', // redirect back to the signup page if there is an error
+          failureFlash : true // allow flash messages
+      }));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function(req, res) {
+      var user            = req.user;
+      user.local.email    = undefined;
+      user.local.password = undefined;
+      user.save(function(err) {
+          res.redirect('/profile');
+      });
+  });
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+
+  res.redirect('/');
+}
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+//tehya note: database.js is not set up so everything involving the database is commented out
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
+
+const morgan = require('morgan');
+const flash = require('connect-flash');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const session = require('express-session');
+
+// const configDB = require('./config/database.js');
+
+// var db
+
+// mongoose.connect(configDB.url, (err, database) => {
+//   if (err) return console.log(err)
+//   db = database
+  require('./app/routes.js')(app, passport); //add db when database.js is set up
+// });
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/public', express.static(__dirname + '/public'))
+
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({
+    secret: 'rouletteOfDeath', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.listen(port);
+console.log(`The magic happens on port ${port}`);
