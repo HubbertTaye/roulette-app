3ed32a569c3ce22506e9ff31557e9813dcf01ce5
Tehya Hubbert <taye@Tehyas-MacBook-Pro.local>
2019-04-11 14:35:12 -0400

    updated database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://project:class@game-f1slf.mongodb.net/house?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb+srv://project:class@game-f1slf.mongodb.net/house?retryWrites=true',
     'dbName': 'house'
 };
